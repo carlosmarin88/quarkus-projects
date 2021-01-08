8a35c845e5610d792a6a66bccd34b4d8586e5662
Carlos Alberto Marin <camarin@baufest.com>
2021-01-08 12:12:30 -0300

    se sube finalizacion del tutorial de jwt

--- security-jwt-quickstart/src/main/java/org/acme/security/jwt/TokenSecuredResource.java
+++ security-jwt-quickstart/src/main/java/org/acme/security/jwt/TokenSecuredResource.java
@@ -1,16 +1,44 @@
 package org.acme.security.jwt;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/secured")
+import org.eclipse.microprofile.jwt.Claim;
+import org.eclipse.microprofile.jwt.Claims;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+
+@Path("/jwt")
+/**
+ * los atributos son propia de esta request y no de otro con @RequestScoped
+ * para no ir pisando lo inject por llamadas paralelas
+ */
+@RequestScoped
 public class TokenSecuredResource {
+
+
+    @Inject
+    @Claim(standard = Claims.sub)
+    String sub;
+
+    @Inject
+    JsonWebToken token;
+
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return this.token.getIssuer();
+    }
+
+    @Path("/sub")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sub(){
+        return this.sub;
     }
 }

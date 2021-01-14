0d4662860928d539d09e28aac3f460b7791b408c
Carlos Alberto Marin <camarin@baufest.com>
2021-01-14 11:19:55 -0300

    se sube healthcheck demo

---
+++ getting-started/src/main/java/org/acme/health/LivenessHealthCheck.java
@@ -0,0 +1,30 @@
+package org.acme.health;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+public class LivenessHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        
+        HealthCheckResponseBuilder builder = HealthCheckResponse.named("WorldClockAPI");
+        
+        try (Socket socket = new Socket()){
+            socket.connect(new InetSocketAddress("worldclockapi.com", 80), 1000);
+            builder.up();
+        } catch (IOException e) {
+            builder.down();
+        }
+
+        return builder.build();
+    }
+    
+}
---
+++ getting-started/src/main/java/org/acme/health/ReadnessHealthCheck.java
@@ -0,0 +1,31 @@
+package org.acme.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.HealthCheckResponseBuilder;
+import org.eclipse.microprofile.health.Readiness;
+
+@Readiness
+public class ReadnessHealthCheck implements HealthCheck {
+
+
+    @Override
+    public HealthCheckResponse call() {
+        
+        HealthCheckResponseBuilder builder = HealthCheckResponse.named("Greeting up");
+        
+        if(databaseIsUp()){
+            builder.up();
+        }else{
+            builder.down();
+            builder.withData("fallo", "Base de datos no arrancada");
+        }
+
+        return builder.build();
+    }
+
+    private boolean databaseIsUp() {
+        return true;
+    }
+    
+}

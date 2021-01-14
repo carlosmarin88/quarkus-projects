db5ed7f79d48a6ec49b8c2f6470c857d5336d671
Carlos Alberto Marin <camarin@baufest.com>
2021-01-14 09:42:19 -0300

    se sube configuracion de circuit breaker

--- getting-started/src/main/java/org/acme/services/WorldClockService.java
+++ getting-started/src/main/java/org/acme/services/WorldClockService.java
@@ -13,6 +13,7 @@ import javax.ws.rs.core.MediaType;
 import org.acme.dto.WorldClock;
 import org.acme.dto.WorldClockFallback;
 import org.acme.headers.WorldClockHeaders;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
@@ -32,6 +33,10 @@ public interface WorldClockService {
     @Retry(maxRetries = 2, delay = 1, delayUnit = ChronoUnit.SECONDS)
     //devuelvo una respuesta predefinida despues de reintentar lo que haya configurado
     @Fallback(WorldClockFallback.class)
+
+    //llamadas consecutivas dentro de una ventana en este caso las 4 ultimas,
+    //despues analizamos el porcentaje de fallo para abrir 
+    @CircuitBreaker(requestVolumeThreshold =  4, failureRatio = 0.75, delay = 1000)
     //aca seteamos datos en el hedaer
     //@ClientHeaderParam(name = "X-Logger", value = "DEBUG")
     //Otra manera de settear header con un objeto
